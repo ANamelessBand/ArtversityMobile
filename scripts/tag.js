1af512125e3125c6a278e4cadc8305f9405b7cbe
Кристиан Ташков <kristian.tashkov@gmail.com>
2014-04-26 19:45:13 +0300

    Tie nearby artists to new tag view

--- scripts/tag.js
+++ scripts/tag.js
@@ -2,29 +2,7 @@
     var TagViewModel,
         app = global.app = global.app || {};
 
-    TagViewModel = kendo.data.ObservableObject.extend({
-    	isNewTag: false,
-
-    	show: function(e) {
-    		var position = app.mapService.viewModel.get("position");
-    		if(position && !e.view.params.forceNewTag) {
-    			kendo.mobile.application.showLoading();
-	    		$.ajax({
-	              url: app.serverEndpoint + "/performances/nearby/" + position.latitude + "/" + position.longitude
-	            }).done(function(data) {
-	            	//
-	            })
-	            .fail(function() {
-	            	app.tagService.viewModel.set("isNewTag",true);
-	            })
-	            .complete(function() {
-	            	kendo.mobile.application.hideLoading();
-	            })
-	        } else {
-        		app.tagService.viewModel.set("isNewTag",true);
-	        }
-    	}
-    });
+    TagViewModel = kendo.data.ObservableObject.extend();
 
     app.tagService = {
         viewModel: new TagViewModel()
