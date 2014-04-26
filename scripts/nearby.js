f33f7e4c610f86f27567bf51cd2db70a1f3c83a4
fo <fouks.f@gmail.com>
2014-04-26 13:30:04 +0300

    Nearby performances view

---
+++ scripts/nearby.js
@@ -0,0 +1,33 @@
+(function (global) {
+    var NearbyViewModel,
+        app = global.app = global.app || {};
+
+    NearbyViewModel = kendo.data.ObservableObject.extend({
+        nearbyDataSource: null,
+
+        init: function () {
+            var that = this,
+                dataSource;
+
+            kendo.data.ObservableObject.fn.init.apply(that, []);
+            //$.get("some-url-here", function(data) {
+                dataSource = new kendo.data.DataSource({
+                    transport: {
+                        read: {
+                            url: "data/nearby.json",
+                            dataType: "json"
+                        }
+                    }
+                });
+
+                console.dir(dataSource);
+                that.set("nearbyDataSource", dataSource);
+            //}));
+        }
+
+    });
+
+    app.nearbyService = {
+        viewModel: new NearbyViewModel()
+    };
+})(window);
