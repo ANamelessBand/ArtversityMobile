67cedd4dbadafdbeb6c9f33ebd3ac5b896c59891
Georgi Gardev <georgi@gardev.com>
2014-04-26 11:10:34 +0300

    Remove unnecessary code in index. Organize

--- scripts/app.js
+++ scripts/app.js
@@ -3,21 +3,9 @@
 
     document.addEventListener('deviceready', function () {
         navigator.splashscreen.hide();
-
-        app.changeSkin = function (e) {
-            var mobileSkin = "";
-
-            if (e.sender.element.text() === "Flat") {
-                e.sender.element.text("Native");
-                mobileSkin = "flat";
-            } else {
-                e.sender.element.text("Flat");
-                mobileSkin = "";
-            }
-
-            app.application.skin(mobileSkin);
-        };
-
-        app.application = new kendo.mobile.Application(document.body, { layout: "tabstrip-layout" });
+        app.application = new kendo.mobile.Application(document.body, {
+          skin:   "flat",
+          layout: "tabstrip-layout"
+        });
     }, false);
 })(window);
