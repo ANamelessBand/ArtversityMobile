42fdd0806f1a3dd8b7fdd22014fd4b9a4ed781be
Georgi Gardev <georgi@gardev.com>
2014-04-27 14:10:05 +0300

    Go Back when tapping fullscreen image. Minor style changes in nearby screen

--- scripts/view_image.js
+++ scripts/view_image.js
@@ -19,6 +19,10 @@
         app.imageViewService.viewModel.setURL(e.view.params.url);
         $(".km-tabstrip").hide();
         $(".km-header").hide();
+
+        $('img.fullscreen-picture').click(function() {
+          kendo.mobile.application.navigate("#:back");
+        });
       },
 
       hide: function() {
